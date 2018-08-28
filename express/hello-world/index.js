b5492687232591c6978554753437ce65d5639eec
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2018-08-28 11:30:20 -0700

    Routing

--- express/hello-world/index.js
+++ express/hello-world/index.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function(req, res) {
-   res.send("Hello world!");
-});
-app.post('/hello', function(req, res) {
-   res.send("Post of the hello is called!");
-});
-app.all('/test', function(req, res) {
-   res.send("HTTP method doesn't have any effect on this route!!");
-});
+var things = require('./things.js');
+app.use('/things', things);
+
 app.listen(3000);
