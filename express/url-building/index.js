984e43be5b64221d9575d7e4cafbdab907b280d7
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2018-08-29 20:18:41 -0700

    url  building 1

--- express/url-building/index.js
+++ express/url-building/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/:id', function(req, res) {
-	res.send('The id you specified is ' + req.params.id);
+app.get('/things/:name/:id', function(req, res) {
+	res.send('id is ' + req.params.id + ' name is ' + req.params.name);
 });
 
 app.listen(3000);
