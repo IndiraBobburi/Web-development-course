2f194a450d8a3f0b646d81f67c5afdfe66d39bf1
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2018-08-30 10:55:23 -0700

    url with regex

--- express/url-building/index.js
+++ express/url-building/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/things/:name/:id', function(req, res) {
-	res.send('id is ' + req.params.id + ' name is ' + req.params.name);
+//only match the requests that have a 5-digit long id.
+app.get('/things/:id([0-9]{5})', function(req, res) {
+	res.send('id is '+ req.params.id);
+});
+
+//invalid url message for other routes
+app.get('*', function(req, res) {
+	res.send('Sorry, this is an invalid URL.');
 });
 
 app.listen(3000);
